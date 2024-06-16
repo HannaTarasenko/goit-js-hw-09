0d9350c9c23cd258f8b6f97df3b128a3861a3fac
HannaTarasenko <155656804+HannaTarasenko@users.noreply.github.com>
2024-06-16 00:27:44 +0000

    Deploying to gh-pages from @ HannaTarasenko/goit-js-hw-09@bc7798c23d8ce9181f74f89b9caf689e2437117b 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector(".feedback-form");document.querySelectorAll(".feedback-form label");e.querySelector('input[name="email"]');e.querySelector('textarea[name="message"]');document.querySelector("button");
+//# sourceMappingURL=commonHelpers2.js.map
